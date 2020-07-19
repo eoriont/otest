6a84a9d852cb803605e75f8aff31e7376ff4b1df
oriont <elijahotarr@gmail.com>
2020-07-18 18:13:44 -0700

    add a singlular .

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A simple package to manage assertions well",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/eoriont/otest",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
